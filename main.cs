c5bd0d8b0aa2ac8b938028395404c4f2d027a36d
konvstheworldd <33183769-konvstheworldd@users.noreply.replit.com>
2024-04-01 21:39:32 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+    string name="Gerrick";
+
+    int mYNum= 47; 
+    
+    Console.WriteLine (name);
+    Console.WriteLine (mYNum);
+  
+
+    
+  }
+}
