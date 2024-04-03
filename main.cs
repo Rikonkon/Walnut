d29b90baaa40350794d729136b60d64396156066
Rikonkon <156250463+Rikonkon@users.noreply.github.com>
2024-04-03 21:42:38 +0000

    Did Conditional Statements

--- main.cs
+++ main.cs
@@ -2,15 +2,21 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-
+    //Variables and Datatypes
     string name="Gerrick";
 
     int mYNum= 47; 
     
     Console.WriteLine (name);
     Console.WriteLine (mYNum);
-  
 
+    //Conditional Statement
+    int Playerhealth= 1;
+    if(Playerhealth == 1) {
+      Console.WriteLine ("Player is Alive");
+    } else {
+      Console.WriteLine ("Player is Dead");
+    }
     
   }
 }
